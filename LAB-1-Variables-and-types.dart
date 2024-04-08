f68e293aa8fe304e618f6947c5267ab30d82ea50
ldmoti <saltyanalysis89@gmail.com>
2024-04-09 00:07:22 +0300

    added variables and types file

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Exercise 1
+  int a = 5;
+  int b = 3;
+  int c = 4;
+  print(a + b + c);
+
+  // Exercise 2
+  String myStr = "Hello, Teacher!";
+  print("the length of myStr is ${myStr.length}");
+}
